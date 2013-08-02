d6288aa02ae945d8ac2e416e220dc84f8482ca46
georgi <gvmitrev@gmail.com>
2013-08-02 15:00:33 +0300

    Barely working version

---
+++ spec/functional/hallo_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "Hallo" do
+
+  context "file with a single transaction"
+  context "document with multiple transactions"
+  context "one document"
+  context "multiple documents"
+end
